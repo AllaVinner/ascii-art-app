19a7dff1ace1952a628574975c5e4a31c8da3fd1
AllaVinner <joel.w.ottosson@gmail.com>
2023-09-30 22:43:50 +0200

    added wasm src pkg folder

---
+++ wasm-src/pkg/wasm_src.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} a
+*/
+export function greet(a: number): void;
+/**
+* @param {Int32Array} v
+*/
+export function add1(v: Int32Array): void;
+/**
+* @param {Uint8Array} data
+*/
+export function printImage(data: Uint8Array): void;
+/**
+* @param {Uint8Array} data
+* @param {number} num_ascii_rows
+* @returns {string}
+*/
+export function convertImage(data: Uint8Array, num_ascii_rows: number): string;
---
+++ wasm-src/pkg/wasm_src_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function greet(a: number): void;
+export function add1(a: number, b: number): void;
+export function printImage(a: number, b: number): void;
+export function convertImage(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
